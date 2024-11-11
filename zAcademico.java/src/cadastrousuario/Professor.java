d0685e1374e9de6afb9173947a8461357255866d
Mateus juan <169060996+MateusJuan@users.noreply.github.com>
2024-11-11 11:48:23 -0300

    11/11/2024

---
+++ zAcademico.java/src/cadastrousuario/Professor.java
@@ -0,0 +1,16 @@
+package cadastrousuario;
+
+public class Professor {
+    private String siape;
+    private String areaFormacao;
+    private String  nivelInstrucao;
+
+
+    public Professor(String siape, String areaFormacao, String nivelInstrucao) {
+        this.siape = siape;
+        this.areaFormacao = areaFormacao;
+        this.nivelInstrucao = nivelInstrucao;
+    }
+    
+
+}
