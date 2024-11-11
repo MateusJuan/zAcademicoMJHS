1ac54b1cf2859cd627feba73240138a77dc44f8a
Mateus juan <169060996+MateusJuan@users.noreply.github.com>
2024-11-11 11:30:07 -0300

    11/11/2024

--- zAcademico.java/src/App.java
+++ zAcademico.java/src/App.java
@@ -1,7 +1,8 @@
 import cadastrousuario.Aluno;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) 
+    throws Exception {
         Aluno Aluno1 = new Aluno("Maria da Silva","01234567890",15,"maria.silva@gmail.com","ms23@jaboatao.ifpe.edu.br","123456","20231TDS-JG0001")
 
         System.out.println(Aluno1.getNome());
