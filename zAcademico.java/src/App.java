f8bd8bf3184b73a0064be57849c806319c013248
Mateus juan <169060996+MateusJuan@users.noreply.github.com>
2024-11-25 12:41:14 -0300

    25/11/2024

--- zAcademico.java/src/App.java
+++ zAcademico.java/src/App.java
@@ -2,13 +2,27 @@ import cadastrousuario.Aluno;
 import cadastrousuario.Escola;
 
 public class App {
-    public static void main(String[] args) 
-    throws Exception {
-        Aluno Aluno1 = new Aluno(" Alice Maria da Silva","01234567890",15,"maria.silva@gmail.com","ms23@jaboatao.ifpe.edu.br","123456","20231TDS-JG0001")
+    public static void main(String[] args) throws Exception {
+        Escola escolaantiga = new Escola("12345560","Escolinha do Pinóqui","Rua do Gepeto",2022,"Maria das Graças ,Diretora,Tel 81 955447755");
 
-        System.out.println(Aluno1.getNome());
-        System.out.println(Aluno1.getIdade());
+        aluno1.setEscolaAnterior(escolaAntiga);
 
-        Escola escola_fundamental = new Escola()
+        aluno1.getEscolaAnterior().imprimeDados();
+
+        ArrayList<NotaSelecao> netaSel = new ArrayList<NotaSelecao>();
+
+        NotaSelecao notaPort = new NotaSelecao("Portguês", 8.5,false);
+        NotaSelecao notaMat = new NotaSelecao("Matemática",7,true);
+        NotaSelecao notaGeo = new NotaSelecao("geografia",8,false);
+
+        notaSel.add(notaPort);
+        notaSel.add(notaMat);
+        notaSel.add(notaGeo);
+
+
+        String validacao = aluno1.validacaoEmailInstitucional(aluno1.getEmailInstitucional());
+        if(validacao==""){
+            System.out.println("E-mail Inválido!!");
+        }
     }
 }
