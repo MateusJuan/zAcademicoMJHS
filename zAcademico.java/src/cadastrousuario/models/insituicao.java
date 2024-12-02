bafd07373f283ea6b77879695b74f1f5caf56106
Mateus juan <169060996+MateusJuan@users.noreply.github.com>
2024-12-02 11:30:35 -0300

    02/11/2024

---
+++ zAcademico.java/src/cadastrousuario/models/insituicao.java
@@ -0,0 +1,8 @@
+package cadastrousuario;
+
+
+public interface insituicao {
+    public void printInfo();
+    public String validaIdMec();
+    public boolean trocouEndereco(String novoEndereco);
+}
